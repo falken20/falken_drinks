3c0629622eab00a3a14496dbf35a8276d85cbbb9
Richi <r.rodriguez@bbva.com>
2025-03-26 07:27:18 +0100

    Setting DB model

--- falken_drinks/models.py
+++ falken_drinks/models.py
@@ -1,5 +1,5 @@
 # by Richi Rod AKA @richionline / falken20
-# ./falken_plants/models.py
+# ./falken_drinks/models.py
 
 # ######################################################################
 # This file is to set all the db models and use the ORM flask_sqlalchemy
@@ -24,113 +24,10 @@ print("Loading models.py")
 db = SQLAlchemy()
 
 
-class Plant(db.Model):
-    __tablename__ = "t_plant"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    name_tech = db.Column(db.String(100), nullable=True)
-    comment = db.Column(db.String(200), nullable=True)
-    # Frequency per month (1-4)
-    watering_summer = db.Column(db.Integer, nullable=True, default=1)
-    # Frequency per month (1-4)
-    watering_winter = db.Column(db.Integer, nullable=True, default=2)
-    spray = db.Column(db.Boolean, nullable=True, default=True)
-    # Direct sun value 1: No, 2: Partial, 3: Yes
-    direct_sun = db.Column(db.Integer, nullable=True, default=2)
-    # image = db.Column(db.BLOB, nullable=True)
-    image = db.Column(db.String(200), nullable=True)
-    date_created = db.Column(db.Date, nullable=False, default=date.today)
-    date_updated = db.Column(db.Date, nullable=False,
-                             default=date.today, onupdate=date.today)
-    user_id = db.Column(db.Integer, db.ForeignKey('t_user.id'), nullable=False)
-
-    def __repr__(self) -> str:
-        return f"<Plant ({self.id} - {self.name} - {self.name_tech})>"
-
-    def __str__(self) -> str:
-        return f"<Plant ({self.id} - {self.name} - {self.name_tech})>"
-
-    def __init__(self, name=None, name_tech=None, comment=None, watering_summer=1, watering_winter=2,
-                 spray=True, direct_sun=2, image=None, user_id=None, date_created=date.today(),
-                 date_updated=date.today()):
-        self.name = name
-        self.name_tech = name_tech
-        self.comment = comment
-        self.watering_summer = watering_summer
-        self.watering_winter = watering_winter
-        self.spray = spray
-        self.direct_sun = direct_sun
-        self.image = image
-        self.user_id = user_id
-        self.date_created = date_created
-        self.date_updated = date_updated
-
-    # Validations => https://flask-validator.readthedocs.io/en/latest/index.html
-    # The __declare_last__() hook allows definition of a class level function that is
-    # automatically called by the MapperEvents.after_configured() event, which occurs
-    # after mappings are assumed to be completed and the ‘configure’ step has finished.
-    @classmethod
-    def __declare_last__(cls):
-        ValidateString(cls.name, False, True,
-                       "Validate String: Plant name can't be empty or only spaces")
-        ValidateInteger(cls.watering_summer, False, True,
-                        "Validate Integer: Plant watering summer should be a number between 0 and 4")
-        ValidateLessThanOrEqual(cls.watering_summer, 4, True,
-                                "ValidateLessThanOrEqual: Plant watering summer should be a number between 0 and 4")
-        ValidateGreaterThanOrEqual(cls.watering_summer, 0, True,
-                                   "ValidateGreaterThanOrEqual: Plant watering summer should be a number between 0 and 4")
-        ValidateInteger(cls.watering_winter, False, True,
-                        "Plant watering winter should be a number between 0 and 4")
-        ValidateLessThanOrEqual(cls.watering_winter, 4, True,
-                                "Plant watering winter should be a number between 0 and 4")
-        ValidateGreaterThanOrEqual(cls.watering_winter, 0, True,
-                                   "Plant watering winter should be a number between 0 and 4")
-        ValidateBoolean(cls.spray)
-        ValidateInteger(cls.direct_sun, True, True,
-                        "Plant direct sun should be a number between 1 and 3")
-        ValidateLessThanOrEqual(cls.direct_sun, 3, True,
-                                "Plant direct sun should be a number between 1 and 3")
-        ValidateGreaterThanOrEqual(cls.direct_sun, 1, True,
-                                   "Plant direct sun should be a number between 1 and 3")
-
-    # SQLAlchemy has a built in helped method for using validations, validates(). Its a Server-side validation
-    @validates('name')
-    def validate_name(self, key, value):
-        if not value:
-            raise ValueError("Plant name can't be empty")
-        return value
-
-    # Check to use @validates('name_tech', 'comment') => https://stackoverflow.com/a/57294872
-    @validates('name_tech', 'comment')
-    def validate_empty_string(self, key, value):
-        if isinstance(value, str) and value == '':
-            return None
-        else:
-            return value
-
-    # Check to use serialize()
-    # How to serialize SqlAlchemy PostgreSQL query to JSON/dict => https://stackoverflow.com/a/46180522
-    def serialize(self):
-        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
-
-
-class Calendar(db.Model):
-    __tablename__ = "t_calendar"
-
-    date_calendar = db.Column(db.Date, primary_key=True)
-    water = db.Column(db.Boolean, nullable=False, default=False)
-    fertilize = db.Column(db.Boolean, nullable=False, default=False)
-    plant_id = db.Column(db.Integer, db.ForeignKey(
-        't_plant.id'), nullable=False)
-
-    def __repr__(self) -> str:
-        return f"<Calendar ({self.date_calendar})>"
-
-
 # Flask-Login can manage user sessions. UserMixin will add Flask-Login attributes
 # to the model so that Flask-Login will be able to work with it.
 class User(UserMixin, db.Model):
-    __tablename__ = "t_user"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
@@ -163,7 +60,135 @@ class User(UserMixin, db.Model):
     # How to serialize SqlAlchemy PostgreSQL query to JSON => https://stackoverflow.com/a/46180522
     def serialize(self):
         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+    
 
+class Drink(db.Model):
+    __tablename__ = "drinks"
+    drink_id = db.Column(db.Integer, primary_key=True)
+    drink_name = db.Column(db.String(100), nullable=False)
+    drink_water_percentage = db.Column(db.Integer, nullable=False, max=100, default=100)
+    drink_alcohol_percentage = db.Column(db.Integer, nullable=False, max=100, default=0)
+    drink_image = db.Column(db.String(100), nullable=True)
+
+    def __repr__(self) -> str:
+        return f"<Drink ({self.drink_name} - {self.drink_water_percentage}% - {self.drink_alcohol_percentage}%)>"
+    
+    def __str__(self) -> str:
+        return f"<Drink ({self.drink_name} - {self.drink_water_percentage}% - {self.drink_alcohol_percentage}%)>"
+    
+    def __init__(self, drink_name=None, drink_water_percentage=None, drink_alcohol_percentage=None, drink_image=None):
+        self.drink_name = drink_name
+        self.drink_water_percentage = drink_water_percentage
+        self.drink_alcohol_percentage = drink_alcohol_percentage
+        self.drink_image = drink_image
+
+    @validates('drink_name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("Drink name can't be empty")
+        return value
+    
+    @validates('drink_water_percentage', 'drink_alcohol_percentage')
+    def validate_percentage(self, key, value):
+        if not value:
+            raise ValueError("Drink percentage can't be empty")
+        return value
+    
+    def serialize(self):
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+    
+    # Validations => https://flask-validator.readthedocs.io/en/latest/index.html
+    # The __declare_last__() hook allows definition of a class level function that is
+    # automatically called by the MapperEvents.after_configured() event, which occurs
+    # after mappings are assumed to be completed and the ‘configure’ step has finished.    
+    @classmethod
+    def __declare_last__(cls):
+        ValidateString(cls.drink_name, False, True,
+                       "Validate String: Drink name can't be empty or only spaces")
+        ValidateInteger(cls.drink_water_percentage, False, True,
+                        "Validate Integer: Drink water percentage should be a number between 0 and 100")
+        ValidateLessThanOrEqual(cls.drink_water_percentage, 100, True,
+                                "ValidateLessThanOrEqual: Drink water percentage should be a number between 0 and 100")
+        ValidateGreaterThanOrEqual(cls.drink_water_percentage, 0, True,
+                                   "ValidateGreaterThanOrEqual: Drink water percentage should be a number between 0 and 100")
+        ValidateInteger(cls.drink_alcohol_percentage, False, True,
+                        "Drink alcohol percentage should be a number between 0 and 100")
+        ValidateLessThanOrEqual(cls.drink_alcohol_percentage, 100, True,
+                                "Drink alcohol percentage should be a number between 0 and 100")
+        ValidateGreaterThanOrEqual(cls.drink_alcohol_percentage, 0, True,
+                                   "Drink alcohol percentage should be a number between 0 and 100")
+    
+    @validates('drink_image')
+    def validate_empty_string(self, key, value):
+        if isinstance(value, str) and value == '':
+            return None
+        else:
+            return value
+        
+    @validates('drink_water_percentage', 'drink_alcohol_percentage')
+    def validate_percentage(self, key, value):
+        if not value:
+            raise ValueError("Drink percentage can't be empty")
+        return value
+    
+    @validates('drink_image')
+    def validate_empty_string(self, key, value):
+        if isinstance(value, str) and value == '':
+            return None
+        else:
+            return value
+    
+
+class DrinkLog(db.Model):
+    __tablename__ = "drinks_logs"
+    log_id = db.Column(db.Integer, primary_key=True)
+    drink_id = db.Column(db.Integer, db.ForeignKey('drinks.drink_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('t_user.id'), nullable=False)
+    date_created = db.Column(db.Date, nullable=False, default=date.today)
+    drink_total_quantity = db.Column(db.Integer, nullable=False)
+    drink_water_quantity = db.Column(db.Integer, nullable=False)
+    drink_alcohol_quantity = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<DrinkLog ({self.date_created} - {self.drink_total_quantity} - {self.drink_water_quantity} - {self.drink_alcohol_quantity})>"
+    
+    def __str__(self) -> str:
+        return f"<DrinkLog ({self.date_created} - {self.drink_total_quantity} - {self.drink_water_quantity} - {self.drink_alcohol_quantity})>"
+    
+    def __init__(self, drink_id=None, user_id=None, drink_total_quantity=None, drink_water_quantity=None, drink_alcohol_quantity=None):
+        self.drink_id = drink_id
+        self.user_id = user_id
+        self.drink_total_quantity = drink_total_quantity
+        self.drink_water_quantity = drink_water_quantity
+        self.drink_alcohol_quantity = drink_alcohol_quantity
+
+    @validates('drink_id', 'user_id')
+    def validate_id(self, key, value):
+        if not value:
+            raise ValueError("Drink ID or User ID can't be empty")
+        return value
+    
+    @validates('drink_total_quantity', 'drink_water_quantity', 'drink_alcohol_quantity')
+    def validate_quantity(self, key, value):
+        if not value:
+            raise ValueError("Drink quantity can't be empty")
+        return value
+    
+    # Validations => https://flask-validator.readthedocs.io/en/latest/index.html
+    # The __declare_last__() hook allows definition of a class level function that is
+    # automatically called by the MapperEvents.after_configured() event, which occurs
+    @classmethod
+    def __declare_last__(cls):
+        ValidateInteger(cls.drink_total_quantity, False, True,
+                        "Drink total quantity should be a number")
+        ValidateInteger(cls.drink_water_quantity, False, True,
+                        "Drink water quantity should be a number")
+        ValidateInteger(cls.drink_alcohol_quantity, False, True,
+                        "Drink alcohol quantity should be a number")
+    
+    def serialize(self):
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+    
 
 def init_db(app):
     """
